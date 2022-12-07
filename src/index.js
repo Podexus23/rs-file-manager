a5fcc9ed7c61f4773b67775ddedf0705474f9a79
Podexus23 <santon23@gmail.com>
2022-12-08 02:12:38 +0300

    feat: add exit fnc for .exit and Ctrl+C

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {argv} from 'process';
+import {argv, stdin, stdout} from 'process';
 
 const checkUserName = () => {
   let name = argv.slice(2)[0];
@@ -6,9 +6,24 @@ const checkUserName = () => {
   else return 'Guest';
 }
 
+const leavingProgram = () => {
+  console.log('Thank you for using File Manager, Username, goodbye!');
+  process.exit();
+}
+
 const mainManager = async() => {
   const USER = checkUserName();
   console.log(`Welcome to the File Manager, ${USER}!`);
+
+  stdin.on('data', (data)=> {
+    const chunkOfData = data.toString('utf-8').trim();
+    console.log(`Data came from terminal: ${data}`);
+    if(chunkOfData === '.exit') leavingProgram();
+  })
+
+  process.on('SIGINT', leavingProgram);
+
 }
+
 
 await mainManager();
