86ca28011b03b232b790c4ea7b562575cc062421
Podexus23 <santon23@gmail.com>
2022-12-19 20:31:43 +0300

    fix: removed useless code from interpreter

--- src/helpers/interpretator.helper.js
+++ src/helpers/interpretator.helper.js
@@ -7,50 +7,44 @@ const inputInterpreter = (string) => {
   const start = {
       quote: null,
       place: null,
-      space: false,
+      space: 0,
   };
-  //holder for spaced commands remove
-  const spaceHolder = {
-    place: 0,
-    space: true,
-  };
-
-
 
   for(let i = 0; i < string.length; i++){
     //quotes solutions
+    //close quote
     if(start.quote === string[i]){
       fullPieces.push(string.slice(start.place + 1, i));
       start.place = null;
       start.quote = null;
       continue;
     }
+    //open quote
     if(QUOTES.includes(string[i]) && start.place === null){
       start.quote = string[i];
       start.place = i;
     }
     //space solutions
     //last check for spaced word
-    if((spaceHolder.space === true) && (i === (string.length - 1)) && !(QUOTES.includes(string[i]))){
-      fullPieces.push(string.slice(spaceHolder.place, i+1));
+    if((i === (string.length - 1)) && !(QUOTES.includes(string[i]))){
+      fullPieces.push(string.slice(start.space, i+1));
     }
     //work with spaced words in middle lane
-    if((spaceHolder.space === true) && (string[i] === ' ') && (start.place === null)){
-      let piece = string.slice(spaceHolder.place, i);
+    if((string[i] === ' ') && (start.place === null)){
+      let piece = string.slice(start.space, i);
       if(!(piece.at(0) && piece.at(-1) && QUOTES.includes(piece.at(0)))){
         fullPieces.push(piece);
       } 
-      spaceHolder.place = i+1;
-      spaceHolder.space = true;
+      start.space = i+1;
     } 
     //first check for spaces without quotes
-    if((spaceHolder.place === 0) && (start.place === null) && (string[i] === ' ')){
-      fullPieces.push(string.slice(spaceHolder.place, i));
-      spaceHolder.place = i+1;
-      spaceHolder.space = true;
+    if((start.space === 0) && (start.place === null) && (string[i] === ' ')){
+      fullPieces.push(string.slice(start.space, i));
+      start.space = i+1;
     }
   }
-  return fullPieces;
+  const trimmed = fullPieces.map(e => e.trim());
+  return trimmed;
 }
 
 export {inputInterpreter};
